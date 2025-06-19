8dc58dc363243d026d86635aa6b4b9019eef4e89
Dhana-shri <sureshdhanashri800@gmail.com>
2025-06-19 15:26:14 +0530

    complete-fxn topic

--- functions.js
+++ functions.js
@@ -26,3 +26,42 @@ const arrowFuncVar = () => {
     console.log("Arrow Function");
 }
 arrowFuncVar();
+
+
+
+//Filter,map,reduce && without loop also fAST
+
+let nums=[1,2,3,4,5];
+let evens=nums.filter((n) => {
+    return n % 2 === 0;
+});
+
+let newEvens=nums.filter((n) => n %2===0);
+
+console.log(evens);
+
+
+let newList=[];
+
+for(let i=0;i<nums.length;i++){
+    if(nums[i]%2===0){
+        newList.push(nums[i]);
+    }
+}
+console.log(newList);
+
+//Map method
+console.log("Using map");
+
+let squares=nums.map((n)=>{
+    return n*n;
+});
+console.log(squares);
+
+//Reduce method
+console.log("using reduce method");
+
+let newSum=nums.reduce((sum,n)=>{
+    return sum+n;
+},(sum=0));
+console.log(newSum);
