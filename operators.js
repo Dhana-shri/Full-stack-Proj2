56a0ea02dc6633af92b8454e79dadd9853d260f0
Dhana-shri <sureshdhanashri800@gmail.com>
2025-06-19 10:54:20 +0530

    complete operators topic

---
+++ operators.js
@@ -0,0 +1,22 @@
+console.log(10+2);
+console.log(10-2);
+console.log(10/2);
+console.log(10*2);
+console.log(10<2);
+console.log(10>2);
+console.log(10<=2);
+console.log(10>=2);
+console.log("2" == 2);
+console.log("2" === 2);
+console.log("2" !== 2);
+
+console.log("Logical operators \n");
+console.log(2>3 && 3>2 );
+console.log(2<3 && 3>2 );
+
+console.log(2>3 || 3>2 );
+console.log(2>3 || 3>2 );
+
+
+
+
