bd8c4316f657d970cd7aa11cef33f65791b4645d
Dhana-shri <sureshdhanashri800@gmail.com>
2025-06-19 11:36:30 +0530

    complete-conditionnal topic

---
+++ conditional.js
@@ -0,0 +1,17 @@
+let marks = 85;
+if (marks >= 90) {
+    console.log("Grade A");
+}
+else if (marks >= 75) {
+    console.log("Grade B");
+}
+else {
+    console.log("Grade C");
+}
+
+if(0){
+    console.log("working");
+}
+else{
+    console.log("Falsy value");
+}
--- operators.js
+++ operators.js
@@ -16,7 +16,3 @@ console.log(2<3 && 3>2 );
 
 console.log(2>3 || 3>2 );
 console.log(2>3 || 3>2 );
-
-
-
-
